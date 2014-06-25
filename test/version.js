e42fae4ab49c59ef52e36fe162552819e5c0f2f0
Gallagher Polyn <gallagher.polyn@gmail.com>
2014-06-24 21:43:53 -0400

    some minimal change to test/version

--- test/version.js
+++ test/version.js
@@ -1,6 +1,6 @@
 casper.test.begin('web app version on index page is correct', 1, function(test) {
     casper.start('http://www.google.fr/', function() {
-        test.assertTextExists('Hello World! My App deployed via Ansible V6.', 'page body mentions correct app version');
+        test.assertTextExists('My App deployed via Ansible V6.', 'page body mentions correct app version');
     }).run(function() {
         test.done();
     });
