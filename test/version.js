7ddd2613d3e862c016ac0058fb443689719877f5
Gallagher Polyn <gallagher.polyn@gmail.com>
2014-06-24 22:46:08 -0400

    some change to test to check for version 5

--- test/version.js
+++ test/version.js
@@ -1,6 +1,6 @@
 casper.test.begin('web app version on index page is correct', 1, function(test) {
-    casper.start('http://www.google.fr/', function() {
-        test.assertTextExists('My App deployed via Ansible V6.', 'page body mentions correct app version');
+    casper.start('http://162.242.211.26:8888', function() {
+        test.assertTextExists('My App deployed via Ansible V5.', 'page body mentions correct app version');
     }).run(function() {
         test.done();
     });
