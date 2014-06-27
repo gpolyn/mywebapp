ab73df15e02eec1e79bec212a2349733720157d2
Gallagher Polyn <gallagher.polyn@gmail.com>
2014-06-26 22:21:15 -0400

    updated version number for index.html in test page

--- test/version.js
+++ test/version.js
@@ -1,6 +1,6 @@
 casper.test.begin('web app version on index page is correct', 1, function(test) {
     casper.start('http://162.222.179.184:8888', function() {
-        test.assertTextExists('My App deployed via Ansible V6.', 'page body mentions correct app version');
+        test.assertTextExists('My App deployed via Ansible V5.', 'page body mentions correct app version');
     }).run(function() {
         test.done();
     });
