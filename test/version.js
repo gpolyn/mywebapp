2477bad47b4fd0e3503f7e55b1ea75f6961091f6
Gallagher Polyn <gallagher.polyn@gmail.com>
2014-06-26 22:09:18 -0400

    updated ip address in test page

--- test/version.js
+++ test/version.js
@@ -1,5 +1,5 @@
 casper.test.begin('web app version on index page is correct', 1, function(test) {
-    casper.start('http://199.223.236.79:8888/', function() {
+    casper.start('http://162.222.179.184:8888', function() {
         test.assertTextExists('My App deployed via Ansible V6.', 'page body mentions correct app version');
     }).run(function() {
         test.done();
